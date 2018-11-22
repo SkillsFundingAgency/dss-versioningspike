993ad2e011903625065937186901203b743bfc9b
Nick Graham101 <nick.graham101@googlemail.com>
2018-11-22 16:30:35 +0000

    Done more work

--- dss-versioningspike/Function1.cs
+++ dss-versioningspike/Function1.cs
@@ -17,6 +17,7 @@ namespace dssversioningspike
 
             //This is version 3 in a branch which has now been changed
             //This is a work in progress feature
+            //I'#e done some more work
 
             // parse query parameter
             string name = req.GetQueryNameValuePairs()
